7040e995898d2843c53d2f6d6f77714271f8071d
Wil LaLonde <wil.lalonde@gmail.com>
2022-09-22 16:03:23 -0500

    commit message

---
+++ Lab2/BusinessLogic.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace Lab2 {
+    public class BusinessLogic : IBusinessLogic {
+        private IDatabase flatDatabase;
+
+        public BusinessLogic() {
+            flatDatabase = new FlatDatabase();
+        }
+
+        public void AddEntry(String clue, String answer, String difficulty, String date) {
+            //Make a call to our "database" after parsing data.
+            //Need to find the new Id value
+            int newId = flatDatabase.GetCurrentCollectionSize();
+            newId++;
+            //Need to parse the difficulty.
+            int newDifficulty = int.TryParse(difficulty, out newDifficulty) ? newDifficulty : Entry.InvalidDifficultyEntry;
+            //Try and make a new Entry
+            Entry addEntry = new Entry(newId, clue, answer, newDifficulty, date);
+            //Check to see if the values were properly added???
+            bool test = true;
+        }
+
+        public void DeleteEntry() {
+            //Make a call to our "database" after parsing data.
+        }
+
+        public void EditEntry() {
+            //Make a call to our "database" after parsing data.
+
+        }
+
+        private String CheckInputEntry(Entry inputEntry) {
+            StringBuilder errorBuilder = new StringBuilder();
+
+            return errorBuilder.ToString();
+        }
+    }
+}
